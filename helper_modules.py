b99575e6e02c94ccfd0833991cb87d256c86a50c
Robin Dupont <robin.dpnt@gmail.com>
2023-03-02 17:48:28 +0000

    correct imports

--- helper_modules.py
+++ helper_modules.py
@@ -1,9 +1,16 @@
 import math
 import torch
+import layer_modules as cl
+
 
 def compute_preds_one_hot(logits):
     _, idx = torch.max(logits, dim=1)
     return idx
 
+
 def layers_with_masks(network):
-    return [ layer for layer in list(network.modules()) if isinstance(layer,(cl.PrunableConv2d,cl.PrunableLinear))]
+    return [
+        layer
+        for layer in list(network.modules())
+        if isinstance(layer, (cl.PrunableConv2d, cl.PrunableLinear))
+    ]
